b0af1e8d2b143182ccd8f0be52713971d56b9b9f
by-German <german280599@gmail.com>
2021-09-24 18:35:19 -0500

    refactor: adding files for job offers service

---
+++ job-offers-service/src/main/java/pe/edu/upc/joboffersservice/JobOffersServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.joboffersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersServiceApplication.class, args);
+    }
+
+}
