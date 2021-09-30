b18c2b43f04416e9548a935867a1cc26f0823551
Rodrigo <rcuenca446@gmail.com>
2021-09-30 00:30:57 -0500

    feature: add delete method

--- src/main/java/upc/edu/pe/projectgradle/controllers/PostulantController.java
+++ src/main/java/upc/edu/pe/projectgradle/controllers/PostulantController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import upc.edu.pe.projectgradle.entity.Postulant;
 import upc.edu.pe.projectgradle.services.PostulantService;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,7 +18,7 @@ public class PostulantController {
 
     @Autowired
     private PostulantService postulantService;
-
+    
     @Operation(summary = "Get postulant by ID", description = "Get postulant by ID", tags={"Postulants"})
     @GetMapping(path = "/postulants/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Postulant> fetchById(@PathVariable("id") Long id) {
@@ -45,6 +46,17 @@ public class PostulantController {
 
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Delete Postulant", tags = {"Postulants"})
+    @DeleteMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Postulant> deletePostulantById(@PathVariable Long id) {
+        try {
+            postulantService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
     }
 
