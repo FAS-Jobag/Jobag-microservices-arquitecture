bea7f35997e16e2b7b68e0a4e246eee49bf8d954
by-German <german280599@gmail.com>
2021-10-01 18:38:10 -0500

    feat: add interviews controller

---
+++ job-offers-service/src/main/java/pe/edu/upc/joboffersservice/services/InterviewService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.joboffersservice.services;
+
+import pe.edu.upc.joboffersservice.entities.Interview;
+import pe.edu.upc.joboffersservice.services.common.CrudService;
+
+public interface InterviewService extends CrudService<Interview, Long> {
+}
---
+++ job-offers-service/src/main/java/pe/edu/upc/joboffersservice/services/impl/InterviewServiceImpl.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.joboffersservice.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.joboffersservice.entities.Interview;
+import pe.edu.upc.joboffersservice.repositories.InterviewRepository;
+import pe.edu.upc.joboffersservice.services.InterviewService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InterviewServiceImpl implements InterviewService {
+    @Autowired
+    private InterviewRepository interviewRepository;
+
+    @Override
+    public Interview save(Interview interview) throws Exception {
+        return interviewRepository.save(interview);
+    }
+
+    @Override
+    public List<Interview> findAll() throws Exception {
+        return interviewRepository.findAll();
+    }
+
+    @Override
+    public Optional<Interview> findById(Long id) throws Exception {
+        return interviewRepository.findById(id);
+    }
+
+    @Override
+    public Interview update(Long id, Interview interview) throws Exception {
+        interview.setId(id);
+        return interviewRepository.save(interview);
+    }
+
+    @Override
+    public void deleteById(Long id) throws Exception {
+        interviewRepository.deleteById(id);
+    }
+}
