6294fc237d12b7554bb5a7fd88a0288650d14e80
Audiomiguel <yomiguelx@gmail.com>
2021-10-01 15:23:46 -0500

    Module project for Postulation Service

---
+++ postulation-service/src/test/java/jobag/postulationservice/PostulationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jobag.postulationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostulationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
