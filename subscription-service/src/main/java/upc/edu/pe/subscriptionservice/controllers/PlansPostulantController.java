537c61cc9e57bf347998e35cbdbdaf5b35f71842
XantX <hp_xantx@hotmail.com>
2021-10-01 20:04:21 -0500

    feature: add controller to plans postulant

--- subscription-service/src/main/java/upc/edu/pe/subscriptionservice/controllers/PlansPostulantController.java
+++ subscription-service/src/main/java/upc/edu/pe/subscriptionservice/controllers/PlansPostulantController.java
@@ -1,5 +1,6 @@
 package upc.edu.pe.subscriptionservice.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -53,6 +54,34 @@ public class PlansPostulantController {
 	public PlanPostulant createPlan (@RequestBody PlanPostulant entity) throws Exception{
 			PlanPostulant plan = entity;
 			return plansPostulantService.save(plan);
+	}
+
+	@DeleteMapping("/plan/id/{id}")
+	public ResponseEntity<PlanPostulant> deleteById(@PathVariable("id") Long id){
+		try {
+			Optional<PlanPostulant>optionalPlanPostulant = plansPostulantService.findById(id);
+			if (optionalPlanPostulant.isPresent()){
+				plansPostulantService.deleteById(id);
+				return new ResponseEntity<PlanPostulant>(optionalPlanPostulant.get(),HttpStatus.OK);
+			}else{
+				return new ResponseEntity<PlanPostulant>(HttpStatus.NOT_FOUND);
+			}
+		} catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@GetMapping("/plans")
+	public ResponseEntity<List<PlanPostulant>> fetchAllEntity(){
+		try {
+			List<PlanPostulant> optionalPlansPostulant = plansPostulantService.findAll();
+			if(!optionalPlansPostulant.isEmpty()){
+				return new ResponseEntity<List<PlanPostulant>>(optionalPlansPostulant,HttpStatus.OK);
+			}else{
+				return new ResponseEntity<List<PlanPostulant>>(HttpStatus.NOT_FOUND);
+			}
+		} catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
