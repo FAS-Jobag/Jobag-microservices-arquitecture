6267c03bd74ff7bcfa891d6edea09194ffbe2731
Mark Albert <markalbert_09@hotmail.com>
2021-10-24 13:00:14 -0500

    feature: configuracion del hystrix

--- postulants-service/src/main/java/upc/edu/pe/projectgradle/ProjectGradleApplication.java
+++ postulants-service/src/main/java/upc/edu/pe/projectgradle/ProjectGradleApplication.java
@@ -3,14 +3,9 @@ package upc.edu.pe.projectgradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-
 public class ProjectGradleApplication {
 
 	public static void main(String[] args) {
