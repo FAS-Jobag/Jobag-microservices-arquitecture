df58bc528bda18d87038c3f1b58d95f43c132811
Audiomiguel <yomiguelx@gmail.com>
2021-11-15 17:56:23 -0500

    test: authentication employers feature

--- GestionEmpleadoresService/src/test/java/upc/edu/pe/gestionempleadoresservice/AutenticacionEmpleadoresTest.java
+++ GestionEmpleadoresService/src/test/java/upc/edu/pe/gestionempleadoresservice/AutenticacionEmpleadoresTest.java
@@ -1,4 +1,37 @@
 package upc.edu.pe.gestionempleadoresservice;
 
-public class AutenticacionEmpleadores {
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import upc.edu.pe.gestionempleadoresservice.entities.Empleador;
+import upc.edu.pe.gestionempleadoresservice.models.User;
+
+public class AutenticacionEmpleadoresTest {
+    Empleador empleador = new Empleador();
+    User user = new User();
+
+    @Test
+    @Given("The employer wants to log in with his/her previous created account")
+    public void the_postulant_wants_to_log_in() {
+        empleador.setCorreo("test@gmail.com");
+        empleador.setContraseña("password");
+    }
+
+    @Test
+    @When("The employer insert his/her authentication's information ")
+    public void the_postulant_inserts_authentication_information(){
+        empleador.setCorreo("test@gmail.com");
+        empleador.setContraseña("password");
+    }
+
+    @Test
+    @Then("The application responds with the employer' primary information ")
+    public void application_responds_postulant_primary_information() throws Throwable{
+
+        Assertions.assertEquals(user.getCorreo(),empleador.getCorreo());
+        Assertions.assertEquals(user.getContraseña(),empleador.getContraseña());
+
+    }
 }
